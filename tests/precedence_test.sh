fc261ab4e08c2a157d67e6cc3d6ae78c3408dac8
Kennen DeRenard <kennen00@gmail.com>
2017-11-15 21:27:07 -0800

    Add test script and files for precedence tests

---
+++ tests/precedence_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+./start.sh
+
+../bin/rshell < inputs/precedence_input.txt > precedence_output.txt
+
+if cmp -s precedence_output.txt expects/precedence_expect.txt; then
+    echo Tests Passed!
+else
+    echo Tests Failed!
+    diff precedence_output.txt expects/precedence_expect.txt
+fi
+
+./clean.sh
